68dab5d9848f8f6b1030aed884b48330a6af9ae0
Hanysisko <tymek@radlowski.net>
2022-01-20 23:18:25 +0100

    last week v1.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import { HomePage } from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shoppage/shoppage.component.jsx';
 
 class App extends Component {
-  constructor() {
-    super();
-  };
+  // constructor() {
+  //   super();
+  // };
   
   render() {
-    return( 
+    return(
       <div className="App">
         <Switch>
           <Route exact path={'/crwn-clothing'} component={ HomePage }/>
-          <Route exact path={'/crwn-clothing/shop/hats'} component={ HatsPage }/>
+          <Route exact path={'/crwn-clothing/shop'} component={ ShopPage }/>
         </Switch>
       </div>
     );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-export const HomePage = () => (
+const HomePage = ({ history }) => (
   
   <div className='homepage'>
+    <button onClick={() => history.push('/crwn-clothing/shop')}>*test* GO TO SHOP *test*</button>
     <Directory/>
   </div>
 
-)
+);
+
+export default withRouter(HomePage);
